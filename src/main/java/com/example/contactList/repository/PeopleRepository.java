838307bf59794d78d80b90efbc242c2ad15a9f68
baranozoglu <abaranozoglu@gmail.com>
2020-12-20 17:36:13 +0300

    search processes has created

--- src/main/java/com/example/contactList/repository/PeopleRepository.java
+++ src/main/java/com/example/contactList/repository/PeopleRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PeopleRepository extends JpaRepository<People, Long> {
-    Page<People> findByName(String name, Pageable pageable);
+    @Query("Select p from People p where lower(p.name) like lower(concat('%', :name,'%'))")
+    Page<People> findByName(@Param("name") String name,@Param("pageable") Pageable pageable);
 }
